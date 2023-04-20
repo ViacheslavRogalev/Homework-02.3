5e12de4bac938a94cb0b2230cc6fb7fd4601f3d3
vyahislav <khv-rvv@mail.ru>
2023-04-21 00:48:31 +1000

    add main

--- main.cpp
+++ main.cpp
@@ -11,37 +11,37 @@ struct address_storage
 	int index;
 };
 
+void print_address_storage (address_storage* address_card)
+{
+	std::cout << "Город: " << address_card->city << std::endl;
+	std::cout << "Улица: " << address_card->street << std::endl;
+	std::cout << "Номер дома: " << address_card->house_number << std::endl;
+	std::cout << "Номер квартиры: " << address_card->apartment_number << std::endl;
+	std::cout << "Индекс: " << address_card->index << "\n" << std::endl;
+}
+
 int main(int argc, char** argv)
 {
 	setlocale(LC_ALL, "Russian");
 
-	address_storage num1;
+	address_storage address_card1;
 
-	num1.city = "Хабаровск";
-	num1.street = "Краснореченская";
-	num1.house_number = 115;
-	num1.apartment_number = 215;
-	num1.index = 680000;
+	address_card1.city = "Хабаровск";
+	address_card1.street = "Краснореченская";
+	address_card1.house_number = 115;
+	address_card1.apartment_number = 215;
+	address_card1.index = 680000;
 
-	std::cout << "Город: " << num1.city << std::endl;
-	std::cout << "Улица: " << num1.street << std::endl;
-	std::cout << "Номер дома: " << num1.house_number << std::endl;
-	std::cout << "Номер квартиры: " << num1.apartment_number << std::endl;
-	std::cout << "Индекс: " << num1.index << "\n\n";
+	address_storage address_card2;
 
-	address_storage num2;
+	address_card2.city = "Владивосток";
+	address_card2.street = "Красноармейская";
+	address_card2.house_number = 208;
+	address_card2.apartment_number = 20;
+	address_card2.index = 690001;
 
-	num2.city = "Владивосток";
-	num2.street = "Красноармейская";
-	num2.house_number = 208;
-	num2.apartment_number = 20;
-	num2.index = 690001;
-
-	std::cout << "Город: " << num2.city << std::endl;
-	std::cout << "Улица: " << num2.street << std::endl;
-	std::cout << "Номер дома: " << num2.house_number << std::endl;
-	std::cout << "Номер квартиры: " << num2.apartment_number << std::endl;
-	std::cout << "Индекс: " << num2.index << "\n\n";
+	print_address_storage(&address_card1);
+	print_address_storage(&address_card2);
 	
 	return 0;
 }
